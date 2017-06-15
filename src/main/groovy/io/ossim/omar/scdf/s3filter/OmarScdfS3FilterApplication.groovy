850ce5c3ec0e11dfe86ca0b80a9dd948d80a35e6
slallier <stephen.lallier@digitalglobe.com>
2017-06-15 15:20:58 -0400

    cleaned up code, version bump to 1.0.3

--- src/main/groovy/io/ossim/omar/scdf/s3filter/OmarScdfS3FilterApplication.groovy
+++ src/main/groovy/io/ossim/omar/scdf/s3filter/OmarScdfS3FilterApplication.groovy
@@ -1,5 +1,6 @@
 package io.ossim.omar.scdf.s3filter
 
+import groovy.util.logging.Slf4j
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.stream.annotation.EnableBinding
@@ -9,31 +10,22 @@ import org.springframework.messaging.Message
 import org.springframework.messaging.handler.annotation.SendTo
 import groovy.json.JsonSlurper
 import groovy.json.JsonBuilder
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 /**
  * Created by cdowin on 6/8/2017
  */
 @SpringBootApplication
 @EnableBinding(Processor.class)
-class OmarScdfS3FilterApplication {
-
+@Slf4j
+class OmarScdfS3FilterApplication
+{
 	/**
-	 * The application logger
-	 */
-	private final Logger logger = LoggerFactory.getLogger(this.getClass())
-
-    OmarScdfS3FilterApplication() {
-
-    }
-
-	/**
-	 * The main entry point of the SCDF Aggregator application.
+	 * The main entry point of the SCDF S3 Filter application.
 	 * @param args
 	 */
-	static final void main(String[] args) {
-		SpringApplication.run OmarScdfS3FilterApplication, args
+	static final void main(String[] args)
+	{
+	       SpringApplication.run OmarScdfS3FilterApplication, args
 	}
 
 	/**
@@ -44,34 +36,31 @@ class OmarScdfS3FilterApplication {
 	 * @param message The message object from the SQS Notifier (in JSON)
 	 * @return a JSON message of the file in S3 and bucket location
 	 */
-	@StreamListener(Processor.INPUT) @SendTo(Processor.OUTPUT)
-	final String filter(final Message<?> message){
+	@StreamListener(Processor.INPUT)
+	@SendTo(Processor.OUTPUT)
+	final String filter(final Message<?> message)
+	{
+        log.debug("Message received: ${message}")
 
-		if(logger.isDebugEnabled()){
-			logger.debug("Message received: ${message}")
-		}
-
-        final def jsonParser = new JsonSlurper()
-
-        try {
-            final def parsedJson = jsonParser.parseText(message)
+        try
+        {
+            final def parsedJson = new JsonSlurper().parseText(message)
             final String bucketName = parsedJson.Records.s3.bucket.name[0]
             final String fileName = parsedJson.Records.s3.object.key[0]
 
             final JsonBuilder parsedJsonS3Data = new JsonBuilder()
             parsedJsonS3Data.file(
-                    bucket: bucketName,
-                    filename: fileName,
+                bucket: bucketName,
+                filename: fileName,
             )
 
-            if (logger.isDebugEnabled()) {
-                logger.debug("Parsed data:\n" + parsedJsonS3Data.toString())
-            }
-
-        } catch (final groovy.json.JsonException jsonEx) {
-            logger.warn("Message received is not in proper JSON format, skipping\n   Message body: ${message}")
+            log.debug("Parsed data:\n" + parsedJsonS3Data.toString())
+    		return parsedJsonS3Data.toString()
         }
-
-		return parsedJsonS3Data.toString()
+        catch (final groovy.json.JsonException jsonEx)
+        {
+            log.warn("Message received is not in proper JSON format, skipping\n   Message body: ${message}")
+    		return null
+        }
 	}
 }
